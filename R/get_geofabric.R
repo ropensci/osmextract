ccc89d0740ab6f899574f0720a6e41b18fb10c34
Andrea <andrea.gilardi@unimib.it>
2019-11-15 12:26:30 +0000

    Improve warning message

--- R/get_geofabric.R
+++ R/get_geofabric.R
@@ -47,7 +47,7 @@ get_geofabric = function(
   } else {
     if(length(name) > 1) {
       name = name[1]
-      warning("Matching only the first name supplied.", immediate. = TRUE)
+      warning("Matching only the first name supplied: ", name, immediate. = TRUE)
     }
     geofabric_matches = gf_find(name, ask, max_dist)
   }
