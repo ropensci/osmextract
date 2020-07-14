b12525b4e2259ba065c9498d9b68a136b84efeb1
Robin Lovelace <rob00x@gmail.com>
2020-07-14 07:54:55 +0100

    Close #71

--- data-raw/geofabrik_zones.R
+++ data-raw/geofabrik_zones.R
@@ -120,6 +120,51 @@ mapview::mapview(res)
 system("ls -hal *.pbf") # 40 kb
 file.copy("its-example.osm.pbf", "inst/")
 
+# update crs of geofabrik zones
+sf::st_crs(geofabrik_zones)
+# Coordinate Reference System:
+#   User input: WGS 84
+# wkt:
+#   GEOGCRS["WGS 84",
+#           DATUM["World Geodetic System 1984",
+#                 ELLIPSOID["WGS 84",6378137,298.257223563,
+#                           LENGTHUNIT["metre",1]]],
+#           PRIMEM["Greenwich",0,
+#                  ANGLEUNIT["degree",0.0174532925199433]],
+#           CS[ellipsoidal,2],
+#           AXIS["geodetic latitude (Lat)",north,
+#                ORDER[1],
+#                ANGLEUNIT["degree",0.0174532925199433]],
+#           AXIS["geodetic longitude (Lon)",east,
+#                ORDER[2],
+#                ANGLEUNIT["degree",0.0174532925199433]],
+#           ID["EPSG",4326]]
+sf::st_crs(geofabrik_zones) = NA
+sf::st_crs(geofabrik_zones)
+sf::st_crs(geofabrik_zones) = 4326
+sf::st_crs(geofabrik_zones)
+# Coordinate Reference System:
+#   User input: EPSG:4326
+# wkt:
+#   GEOGCRS["WGS 84",
+#           DATUM["World Geodetic System 1984",
+#                 ELLIPSOID["WGS 84",6378137,298.257223563,
+#                           LENGTHUNIT["metre",1]]],
+#           PRIMEM["Greenwich",0,
+#                  ANGLEUNIT["degree",0.0174532925199433]],
+#           CS[ellipsoidal,2],
+#           AXIS["geodetic latitude (Lat)",north,
+#                ORDER[1],
+#                ANGLEUNIT["degree",0.0174532925199433]],
+#           AXIS["geodetic longitude (Lon)",east,
+#                ORDER[2],
+#                ANGLEUNIT["degree",0.0174532925199433]],
+#           USAGE[
+#             SCOPE["unknown"],
+#             AREA["World"],
+#             BBOX[-90,-180,90,180]],
+#           ID["EPSG",4326]]
+
 # The end
 usethis::use_data(geofabrik_zones, overwrite = TRUE)
 
