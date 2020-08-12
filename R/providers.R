26ee47f0b8a02eb2e6b051290e4c335abd0cb2a0
Andrea Gilardi <a.gilardi5@campus.unimib.it>
2020-08-12 10:17:05 +0200

    Fix #77 and #99

--- R/providers.R
+++ R/providers.R
@@ -31,7 +31,9 @@ load_provider_data = function(provider) {
     "bbbike" = osmextract::bbbike_zones
     # , "another" = another_provider
   )
-  sf::st_crs(provider_data) = 4326
+  if (is.null(sf::st_crs(provider_data))) {
+    sf::st_crs(provider_data) = 4326
+  }
   provider_data
 }
 
--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,7 @@
 .onAttach = function(libname, pkgname) {
   packageStartupMessage(paste(
-    "Data (c) OpenStreetMap contributors, ODbL 1.0. https://www.openstreetmap.org/copyright",
+    "Data (c) OpenStreetMap contributors, ODbL 1.0. https://www.openstreetmap.org/copyright.",
+    "Any product made from OpenStreetMap must cite OSM as the data source.",
     "Geofabrik data are taken from https://download.geofabrik.de/",
     "For usage details of bbbike data see https://download.bbbike.org/osm/",
     sep = "\n"
