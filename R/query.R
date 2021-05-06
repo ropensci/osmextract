56f6721b5a802920d2a35d6ce8ffe5b8132d4943
Robin Lovelace <rob00x@gmail.com>
2021-05-06 17:29:56 +0100

    Update with vtlist

--- R/query.R
+++ R/query.R
@@ -1,43 +1,45 @@
-#' Convert a list to a character strings representing a query
+#' Convert a list to character strings for gdal_utils options
 #'
-#' @param query_list
+#' @param vtlist
 #'
 #' @return
 #' @export
 #'
 #' @examples
-#' oe_query(query_list = list(overwrite = TRUE))
-#' oe_query(query_list = list(overwrite = TRUE, f = "GPKG"))
-#' oe_query(query_list = list(f = "GPKG", t_srs = "EPSG:32633"))
+#' oe_vtlist(vtlist = list(overwrite = TRUE))
+#' oe_vtlist(vtlist = list(overwrite = TRUE, f = "GPKG"))
+#' oe_vtlist(vtlist = list(f = "GPKG", t_srs = "EPSG:32633"))
 #' its = oe_get("ITS Leeds", quiet = FALSE, download_directory = tempdir())
 #' # mapview::mapview(its[1:5, ]) # find suitable subset
 #' its_sample_union = sf::st_union(its[1:5, ])
 #' its_polygon = sf::st_convex_hull(its_sample_union)
 #' its_poly_txt = sf::st_as_text(its_polygon)
-#' q = oe_query(list(clipdst = its_poly_txt))
+#' spatial_extent = "-1.559184 53.807739 -1.557375 53.808094"
+#' q = oe_vtlist(list(spat = spatial_extent))
+#' q
 #' its2 = oe_get("ITS Leeds", quiet = FALSE, download_directory = tempdir(),
 #'   vectortranslate_options = q)
 #' plot(its$geometry, col = "grey")
 #' plot(its_polygon, add = TRUE)
-oe_query = function(query_list = NULL) {
+oe_vtlist = function(vtlist = NULL, layer = "lines") {
 
   # browser()
-  list_names = names(query_list)
-  list_classes = sapply(query_list, class)
+  list_names = names(vtlist)
+  list_classes = sapply(vtlist, class)
 
-  logical_queries = query_list[list_classes == "logical"]
-  logicals = sapply(names(logical_queries), oe_query_l)
+  logical_queries = vtlist[list_classes == "logical"]
+  logicals = sapply(names(logical_queries), oe_vtlist_l)
 
-  character_queries = query_list[list_classes == "character"]
-  characters = mapply(oe_query_c, names(character_queries), character_queries)
+  character_queries = vtlist[list_classes == "character"]
+  characters = mapply(oe_vtlist_c, names(character_queries), character_queries)
 
   unname(unlist(c(logicals, characters)))
 
 }
-oe_query_l = function(q) {
+oe_vtlist_l = function(q) {
   paste0("--", q)
 }
-oe_query_c = function(v, q) {
+oe_vtlist_c = function(v, q) {
   argument = paste0("-", v)
   c(argument, q)
 }
