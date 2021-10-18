c2cc22d6ea6b8f0d14a2457ce85351b894fb8c91
Andrea <andrea.gilardi@unimib.it>
2021-10-18 09:23:11 +0200

    test on rhub

--- my-rhub-commands.R
+++ my-rhub-commands.R
@@ -2,13 +2,11 @@ library(rhub)
 
 # results = check_for_cran(
 #   platforms = c(
-#     "ubuntu-gcc-release",
-#     "windows-x86_64-devel",
-#     "debian-gcc-devel"
-#   ),
-#   show_status = FALSE
+#     "debian-gcc-devel",
+#     "fedora-gcc-devel"
+#   )
 # )
-#
+
 # previous_checks = rhub::list_package_checks(howmany = 1)
 # id = previous_checks$group[1]
 # group_check = rhub::get_check(id)
