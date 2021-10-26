db3bacc32d46bf1d61dfa43d824ca6185af7cb85
Andrea Gilardi <andrea.gilardi@unimib.it>
2021-10-26 14:09:37 +0200

    update URL for gdal/osmconf.ini after https://github.com/OSGeo/gdal/commit/9e9f8b294418e5f2670dacd5cacb5d6cdeabb797

--- R/vectortranslate.R
+++ R/vectortranslate.R
@@ -29,7 +29,7 @@
 #'   reads and processes a `.osm.pbf` file. More precisely, several operations
 #'   that GDAL performs on the input `.osm.pbf` file are governed by a `CONFIG`
 #'   file, that can be checked at the following
-#'   [link](https://github.com/OSGeo/gdal/blob/master/gdal/data/osmconf.ini).
+#'   [link](https://github.com/OSGeo/gdal/blob/master/data/osmconf.ini).
 #'   The basic components of OSM data are called
 #'   [*elements*](https://wiki.openstreetmap.org/wiki/Elements) and they are
 #'   divided into *nodes*, *ways* or *relations*, so, for example, the code at
@@ -307,7 +307,7 @@ oe_vectortranslate = function(
   if (is.null(osmconf_ini)) {
     # The file osmconf.ini stored in the package is the default osmconf.ini used
     # by GDAL at stored at the following link:
-    # https://github.com/OSGeo/gdal/blob/master/gdal/data/osmconf.ini
+    # https://github.com/OSGeo/gdal/blob/master/data/osmconf.ini
     # It was saved on the 9th of July 2020.
     osmconf_ini = system.file("osmconf.ini", package = "osmextract")
   }
