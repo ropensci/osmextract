5ae2a2f65ddc9e56a8589bfd4900d7350160b954
Andrea Gilardi <andrea.gilardi@unimib.it>
2021-11-03 12:10:21 +0100

    tidy tests + add my-covr.R

---
+++ my-covr.R
@@ -0,0 +1,2 @@
+Sys.setenv(NOT_CRAN = "true")
+covr::report()
--- tests/testthat/test-read.R
+++ tests/testthat/test-read.R
@@ -108,14 +108,14 @@ test_that("extra_tags are not ignored when vectortranslate_options is not NULL",
   file.remove(list.files(tempdir(), pattern = "its-example.gpkg", full.names = TRUE))
 })
 
-test_that("osmconf_ini is not ignored when vectortranslate_options is not NULL", {
-  # Create a fake osmconf_ini
-  custom_osmconf_ini = readLines(system.file("osmconf.ini", package = "osmextract"))
-  custom_osmconf_ini[[18]] = "report_all_nodes=yes"
-  custom_osmconf_ini[[21]] = "report_all_ways=yes"
-  temp_ini = tempfile(fileext = ".ini")
-  writeLines(custom_osmconf_ini, temp_ini)
+# Create a fake osmconf_ini
+custom_osmconf_ini = readLines(system.file("osmconf.ini", package = "osmextract"))
+custom_osmconf_ini[[18]] = "report_all_nodes=yes"
+custom_osmconf_ini[[21]] = "report_all_ways=yes"
+temp_ini = tempfile(fileext = ".ini")
+writeLines(custom_osmconf_ini, temp_ini)
 
+test_that("osmconf_ini is not ignored when vectortranslate_options is not NULL", {
   # Regular output
   its_gpkg = oe_read(its_pbf, quiet = TRUE)
   # Ad hoc osmconf_ini + vectortranslate options
@@ -130,7 +130,9 @@ test_that("osmconf_ini is not ignored when vectortranslate_options is not NULL",
   expect_gte(nrow(its_gpkg_osmconf), nrow(its_gpkg))
   expect_true(sf::st_is_longlat(its_gpkg))
   expect_false(sf::st_is_longlat(its_gpkg_osmconf))
+})
 
+test_that("warning with ad_hoc osmconf_ini + extra_tags", {
   # Warning with adhoc osmconf_ini + extra_tags
   expect_warning(
     oe_read(
@@ -141,8 +143,10 @@ test_that("osmconf_ini is not ignored when vectortranslate_options is not NULL",
       extra_tags = "oneway"
     )
   )
+})
 
-  # Warning with ad-hoc osmconf_ini + CONFIG_FILE in vectortranslate_options
+test_that("warning with ad-hoc osmconf_ini + CONFIG_FILE in vectortranslate_options", {
+  # Warning
   expect_warning(
     oe_read(
       its_pbf,
@@ -151,53 +155,59 @@ test_that("osmconf_ini is not ignored when vectortranslate_options is not NULL",
       vectortranslate_options = c("-t_srs", "EPSG:27700", "-oo", paste0("CONFIG_FILE=", temp_ini)),
     )
   )
-
-  # clean tempdir
-  file.remove(list.files(tempdir(), pattern = "its-example.gpkg", full.names = TRUE))
 })
 
-test_that("boundary and boundary_type arguments from oe_vectortranslate works", {
-  # Define a small polygon in the area of ITS Leeds
-  its_poly = sf::st_sfc(
-    sf::st_polygon(
-      list(rbind(
-        c(-1.55577, 53.80850),
-        c(-1.55787, 53.80926),
-        c(-1.56096, 53.80891),
-        c(-1.56096, 53.80736),
-        c(-1.55675, 53.80658),
-        c(-1.55495, 53.80749),
-        c(-1.55577, 53.80850)
-      ))
-    ),
-    crs = 4326
-  )
+# Define a small polygon in the area of ITS Leeds
+its_poly = sf::st_sfc(
+  sf::st_polygon(
+    list(rbind(
+      c(-1.55577, 53.80850),
+      c(-1.55787, 53.80926),
+      c(-1.56096, 53.80891),
+      c(-1.56096, 53.80736),
+      c(-1.55675, 53.80658),
+      c(-1.55495, 53.80749),
+      c(-1.55577, 53.80850)
+    ))
+  ),
+  crs = 4326
+)
 
+test_that("boundary and boundary_type arguments from oe_vectortranslate works", {
   # Spatial filters work
   its = oe_read(its_pbf, quiet = TRUE)
   its_spat = oe_read(its_pbf, boundary = its_poly %>% sf::st_transform(27700), quiet = TRUE)
   its_clipsrc = oe_read(its_pbf, boundary = its_poly, quiet = TRUE, boundary_type = "clipsrc")
   expect_lte(nrow(its_spat), nrow(its))
   expect_lte(nrow(its_clipsrc), nrow(its_spat))
+})
 
-  # Spatial filters work with bbox objects
+test_that("spatial filters work with bbox objects", {
   its_spat = oe_read(its_pbf, boundary = its_poly %>% sf::st_transform(27700), quiet = TRUE)
   its_spat_bbox = oe_read(its_pbf, boundary = its_poly %>% sf::st_transform(27700) %>% sf::st_bbox(), quiet = TRUE)
   expect_gte(nrow(its_spat_bbox), nrow(its_spat))
+})
 
-  # Can combine with other vectortranslate arguments
+test_that("we can combine boundary = ... with other vectortranslate arguments", {
+  its_spat = oe_read(its_pbf, boundary = its_poly %>% sf::st_transform(27700), quiet = TRUE)
+  its_clipsrc = oe_read(its_pbf, boundary = its_poly, quiet = TRUE, boundary_type = "clipsrc")
   its_clipsrc_small = oe_read(its_pbf, boundary = its_poly, quiet = TRUE, boundary_type = "clipsrc", vectortranslate_options = c("-where", "highway = 'footway'"))
   expect_lte(nrow(its_clipsrc_small), nrow(its_spat))
   expect_equal(nrow(its_clipsrc_small), sum(its_clipsrc[["highway"]] == "footway", na.rm = TRUE))
+})
 
+test_that("get a warning for more than 1 polygon in boundary = ...", {
   # Warning for more than 1 POLYGON
   expect_warning(oe_read(its_pbf, boundary = c(its_poly, its_poly), quiet = TRUE))
+})
 
+test_that("get error when boundary is not a POLYGON/MULTIPOLYGON", {
   # Error for non POLYGON boundary. I need suppressWarnings for the warning on
   # centroids for lat/long data
   suppressWarnings(expect_error(oe_read(its_pbf, boundary = sf::st_centroid(its_poly), quiet = TRUE)))
+})
 
-  # Warning for "-spat"/"-clipsrc" in vectortranslate_options
+test_that("warning when setting boundary and spat/clipsrc options", {
   expect_warning(oe_read(
     file_path = its_pbf,
     boundary = its_poly,
@@ -211,9 +221,8 @@ test_that("boundary and boundary_type arguments from oe_vectortranslate works", 
     vectortranslate_options = c("-clipsrc", sf::st_as_text(its_poly)),
     quiet = TRUE
   ))
-
-  file.remove(list.files(tempdir(), pattern = "its-example.gpkg", full.names = TRUE))
 })
 
 # Clean tempdir
+rm(its_poly)
 file.remove(list.files(tempdir(), pattern = "its-example", full.names = TRUE))
