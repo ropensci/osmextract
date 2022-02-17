88fdc86111a8b0f2b9a165c92cd7f62515a15f60
Andrea Gilardi <andrea.gilardi@unimib.it>
2022-02-17 15:57:50 +0100

    Revert "delete tests for oe_update for the moment"
    This reverts commit d8a9ea753c8f5223a133a85ae477a4fb90274619.

--- tests/testthat/test-update.R
+++ tests/testthat/test-update.R
@@ -3,10 +3,17 @@ test_that("oe_update(): simplest example works", {
   skip_on_cran()
   skip_if_offline("github.com")
 
-  # expect_error(oe_update(tempdir(), quiet = TRUE), NA)
+  out = oe_get(
+    "ITS Leeds",
+    provider = "test",
+    download_directory = tempdir(),
+    download_only = TRUE,
+    quiet = TRUE
+  )
+  expect_error(oe_update(tempdir(), quiet = TRUE), NA)
 
   # AG: I decided to comment out that test since I don't see any benefit testing
   # the "verbose" output during R CMD checks (that I rarely check manually)
   # expect_message(oe_update(fake_dir, quiet = FALSE))
-  # file.remove(list.files(tempdir(), pattern = "its-example", full.names = TRUE))
+  file.remove(list.files(tempdir(), pattern = "its-example", full.names = TRUE))
 })
