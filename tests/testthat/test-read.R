25e7910a25e2d0cfab57b66beeec27b3349f58b2
Andrea Gilardi <andrea.gilardi@unimib.it>
2026-03-14 11:00:01 +0100

    reinstate test

--- tests/testthat/test-read.R
+++ tests/testthat/test-read.R
@@ -234,18 +234,18 @@ test_that("we can combine boundary = ... with other vectortranslate arguments", 
   expect_equal(nrow(its_clipsrc_small), sum(its_clipsrc[["highway"]] == "footway", na.rm = TRUE))
 })
 
-# test_that("get a warning for more than 1 polygon in boundary = ...", {
-#   withr::local_envvar(
-#     .new = list(
-#       "OSMEXT_DOWNLOAD_DIRECTORY" = tempdir(),
-#       "TESTTHAT" = "true"
-#     )
-#   )
-#   its_pbf = setup_pbf()
-#
-#   # Warning for more than 1 POLYGON
-#   expect_warning(oe_read(its_pbf, boundary = c(its_poly, its_poly), quiet = TRUE))
-# })
+test_that("get warning for more than 1 polygon in boundary = ...", {
+  withr::local_envvar(
+    .new = list(
+      "OSMEXT_DOWNLOAD_DIRECTORY" = tempdir(),
+      "TESTTHAT" = "true"
+    )
+  )
+  its_pbf = setup_pbf()
+
+  # Warning for more than 1 POLYGON
+  expect_warning(oe_read(its_pbf, boundary = c(its_poly, its_poly), quiet = TRUE))
+})
 
 test_that("get error when boundary is not a POLYGON/MULTIPOLYGON", {
   withr::local_envvar(
